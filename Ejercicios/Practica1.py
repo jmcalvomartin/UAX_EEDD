dcce6336649b4e30ca8915a3255a1c15cdab96b1
Jorge Calvo <jorge@europeanvalley.es>
2022-11-03 15:04:22 +0100

    Update Practica1.py

---
+++ Ejercicios/Practica1.py
@@ -0,0 +1,13 @@
+'''
+Saca por pantalla "Asignatura de TIC" usando variable
+Saca por pantalla lo mismo que antes pero la variable solo puede contener el valor "TIC"
+Saca por pantalla mi nombre es " ", pero solo puedes pedirle al usuario su nombre.
+(OJO elnombre siempre debe salir en mayúscula)
+'''
+
+
+'''
+Le pido al usuario su nombre y su nacionalidad y después saco por pantalla:
+Ejemplo
+"Aitor" tiene la nacionalidad "Francesa"
+'''
