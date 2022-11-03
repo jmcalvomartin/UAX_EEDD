fe8be2ef03e3e017d576ba94a45edc93cfa7109f
Jorge Calvo <jorge@europeanvalley.es>
2022-11-03 16:21:53 +0100

    Update Practica1.py

--- Ejercicios/Practica1.py
+++ Ejercicios/Practica1.py
@@ -4,10 +4,31 @@ Saca por pantalla lo mismo que antes pero la variable solo puede contener el val
 Saca por pantalla mi nombre es " ", pero solo puedes pedirle al usuario su nombre.
 (OJO elnombre siempre debe salir en mayúscula)
 '''
+a="Asignatura de TIC"
+print(a)
 
+b="TIC"
+print("Asignatura de ",b)
+
+name=input("Dime tu nombre: ")
+print("Mi nombre es ",name.upper())
 
 '''
 Le pido al usuario su nombre y su nacionalidad y después saco por pantalla:
 Ejemplo
 "Aitor" tiene la nacionalidad "Francesa"
 '''
+name=input("Dime tu nombre: ")
+nacionalidad=input("Dime la nacionalidad: ")
+
+print(name.upper()," mi nacionalidad es ", nacionalidad.lower())
+
+'''
+Necesito un programa que realice el teorema de Pitágoras. Debe pedirle al usuario lalongitud de los 2 catetos y devolverle el valor de la hipotenusa.
+Tambien debe decirle el valor total del perimetro del triangulo.
+'''
+c1=int(input("Dime un lado: "))
+c2=int(input("Dime otro lado: "))
+
+h=(c1**2+c2**2)**(1/2)
+print("La hipotenusa es ", h , " y el perimetro es ",(c1+c2+h))
